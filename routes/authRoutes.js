6eb93ad211aabd29c53b9ae5d47c26fdbee23627
Mr AiM <tjtask346@gmail.com>
2026-09-12 08:51:17 +0600

    Update auth routes to use Firebase and PIN login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { registerWithFirebase, loginWithPin } = require('../controllers/authController');
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', registerWithFirebase);
+router.post('/login', loginWithPin);
 
 module.exports = router;
