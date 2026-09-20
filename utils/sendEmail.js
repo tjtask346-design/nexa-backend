b849f7cbded5a9206d2e1df351c4fb4ba0b5fe2d
Mr AiM <tjtask346@gmail.com>
2026-09-20 11:04:20 +0600

    Delete utils/sendEmail.js

--- utils/sendEmail.js
+++
@@ -1,6 +0,0 @@
-// Firebase now handles emails, dummy file to prevent crash
-const sendEmail = async (options) => {
-    console.log(`[Email Disabled] To: ${options?.email}`);
-    return { success: true };
-};
-module.exports = sendEmail;
