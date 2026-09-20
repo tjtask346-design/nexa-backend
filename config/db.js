65408c7d4e981bd2c562203dd43faef2e66992ca
Mr AiM <tjtask346@gmail.com>
2026-09-20 10:17:46 +0600

    Improve error messages in database connection

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
-
 const connectDB = async () => {
+    if (!process.env.MONGO_URI) {
+        console.error('❌ MONGO_URI missing');
+        process.exit(1);
+    }
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error(`Database Error: ${error.message}`);
+        console.error(`❌ DB Error: ${error.message}`);
         process.exit(1);
     }
 };
-
 module.exports = connectDB;
-
